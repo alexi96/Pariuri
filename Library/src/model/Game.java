986dbed52cac46f10772ec7c0417d2d20138ae88
ryandruid <rares.ctn@gmail.com>
2017-04-01 17:57:01 +0300

    game

---
+++ Library/src/model/Game.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.io.Serializable;
+
+public class Game implements Serializable, Comparable<Game> {
+
+    protected String name;
+    protected Integer id;
+    protected Float chance;
+    
+    public Game() {
+        
+    }
+    
+    public Game(Integer id) {
+        this.id = id;
+    }
+    
+    public Game(String name, Float chance) {
+        this.name = name;
+        this.chance = chance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Float getChance() {
+        return chance;
+    }
+
+    public void setChance(Float chance) {
+        this.chance = chance;
+    }
+
+}
