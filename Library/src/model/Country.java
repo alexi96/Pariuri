63cda3f0013f97d9cc87b56fca492b4b56191e9c
ryandruid <rares.ctn@gmail.com>
2017-04-01 17:43:17 +0300

    country

--- Library/src/model/Country.java
+++ Library/src/model/Country.java
@@ -1,5 +1,34 @@
 package model;
 
-public class Country {
+import java.io.Serializable;
+
+public class Country implements Serializable, Comparble<Country>{
     
+    protected String name;
+    
+    public Country() {
+        
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public int compareTo(Country c){
+            return this.name.compareTo(c.name);
+    }
+
+    @Override
+    public String toString(){
+        return this.name;
+    }
+
 }
+    
+
+    
