40c729a6e6ef30d28f6d6f5dc695a69bde65a682
alexi96 <alex_i96@yahoo.com>
2017-04-12 17:33:03 +0300

    C

--- Server/src/application/Application.java
+++ Server/src/application/Application.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package application;
 
 import connection.Connection;
@@ -12,10 +7,6 @@ import java.util.logging.Logger;
 import rpc.HiRpc;
 import server.BetServer;
 
-/**
- *
- * @author alex_
- */
 public class Application {
 
     public static void main(String[] args) {
--- Server/src/server/BetServer.java
+++ Server/src/server/BetServer.java
@@ -1,6 +1,8 @@
 package server;
 
 import connection.Connection;
+import db.StatisticTypeDB;
+import db.UserDB;
 import hiper.PersistenceUnit;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -13,8 +15,12 @@ public class BetServer implements Connection {
     public BetServer() {
         try {
             this.pu = new PersistenceUnit("jdbc:mysql://localhost:3306/pariuri", "root", "");
+            this.pu.register(UserDB.class);
+            this.pu.register(StatisticTypeDB.class);
         } catch (SQLException ex) {
             Logger.getLogger(BetServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    
 }
